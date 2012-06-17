072fb128921114628d89b95c0e10a63344dbd4db
Claudio Dawson d'Angelis <claudiodangelis@gmail.com>
2012-06-17 18:01:30 +0200

    commit

--- PokerEngine.dart
+++ PokerEngine.dart
@@ -8,21 +8,16 @@ void serveHand(int player){
       servedCards.add(receivedCard);
       }
     }
-
   Hand playerHand=new Hand(1,servedCards[0],servedCards[1],servedCards[2],servedCards[3],servedCards[4],'Not set');
-
-  
 }
 
 Card pickCard(){
-
   int rnd, randomSuite, randomRank;
   rnd = new Date.now().value;
   randomSuite = ((rnd/(255*Math.random())).toInt()) %4;
   rnd = new Date.now().value;
   randomRank = ((rnd/(255*Math.random())).toInt()) %14;
       if(randomRank==0){randomRank++;}
-      
   Card giveCard = new Card(randomSuite,randomRank);
   return giveCard;
 }
